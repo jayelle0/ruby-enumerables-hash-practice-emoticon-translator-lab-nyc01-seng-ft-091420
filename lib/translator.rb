3c28c54ccd7f63b113b0efd2b658b6ee5ae978c4
Jacqueline <lojacqueline2@gmail.com>
2020-09-11 00:31:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
-    if value [:japanese] == emoticon
+    if value [:english] == emoticon
       return key 
 
     end
